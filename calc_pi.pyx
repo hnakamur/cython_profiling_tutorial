5cd5871f9c774cd5e90a2af52ae327963c8e64c5
Hiroaki Nakamura <hnakamur@gmail.com>
2015-07-11 22:04:20 +0900

    Add type to approx_pi argument and local variable

--- calc_pi.pyx
+++ calc_pi.pyx
@@ -5,8 +5,9 @@
 cdef inline double recip_square(i):
     return 1./i**2
 
-def approx_pi(n=10000000):
-    val = 0.
+def approx_pi(int n=10000000):
+    cdef double val = 0.
+    cdef int k
     for k in range(1,n+1):
         val += recip_square(k)
     return (6 * val)**.5
